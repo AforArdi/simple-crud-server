d21eec9f9f909299be23ec8b32cd8794b43c3600
AforArdi <binsbro36@gmail.com>
2026-05-09 06:59:14 +0600

    specific user query with id implemented

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -22,10 +22,20 @@ const run = async ()=>{
 
     app.get('/users', async (req, res)=>{
       const db = client.db('simple_crud_db');
-      const users = db.collection('users');
-      const cursor = users.find();
+      const userCollection = db.collection('users');
+      const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+
+      app.get('/users/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = {
+          _id: new ObjectId(id)
+        };
+        const result = await userCollection.findOne(query);
+        res.send(result);
+      })
+      
     })
 
     await client.db("admin").command({ ping: 1 });
