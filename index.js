639f43a76e694d77977aa9003bd740fbebf34de3
AforArdi <binsbro36@gmail.com>
2026-05-09 06:17:20 +0600

    connected to mongodb, showing users on api link

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster-ardi.8lva6gn.mongodb.net/?appName=Cluster-Ardi";
+const uri = "mongodb+srv://simple_crud:37OlGbCYE50miKOU@cluster-ardi.8lva6gn.mongodb.net/?appName=Cluster-Ardi";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -16,6 +16,26 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+const run = async ()=>{
+  try{
+    await client.connect();
+
+    app.get('/users', async (req, res)=>{
+      const db = client.db('simple_crud_db');
+      const users = db.collection('users');
+      const cursor = users.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }
+  finally{
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res)=>{
     res.send('Hello from Home')
